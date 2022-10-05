71c45daeb9494ee5f3a6b66c2e9397043e5b6f98
Ethan Li <lietk12@gmail.com>
2022-10-04 22:00:10 -0700

    Retry ACL bootstrapping until Nomad systemd service finishes starting

--- provisioners/hashistack/nomad/configure.sh
+++ provisioners/hashistack/nomad/configure.sh
@@ -5,8 +5,12 @@ sudo cp /tmp/packer-files/hashistack/nomad/*.hcl /etc/nomad.d/
 sudo chmod 644 /etc/nomad.d/*.hcl
 sudo chown nomad:nomad /etc/nomad.d/*.hcl
 
-# Set up systemd
+# Set up systemd service
 sudo systemctl enable nomad.service
+sudo systemctl start nomad.service
+sleep 1
 
 # Bootstrap ACLs
-nomad acl bootstrap /tmp/packer-files/hashistack/nomad/root.token >> /dev/null
+until nomad acl bootstrap /tmp/packer-files/hashistack/nomad/root.token >> /dev/null; do
+  sleep 1
+done
